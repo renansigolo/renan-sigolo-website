ada5991fc55b5838a15b7542ccb55c7132961807
Renan Sigolo <renan.sigolo@gmail.com>
2021-03-18 09:08:21 +1100

    build: update dependencies

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ module.exports = {
 }
 
 // PWA
-// const withPWA = require('next-pwa')
-// module.exports = withPWA({
-//   pwa: {
-//     disable: process.env.NODE_ENV === 'development',
-//     dest: 'public',
-//   },
-// })
+const withPWA = require('next-pwa')
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+  },
+})
