10c25adc683381fd560f42942f347e1f279d62d4
Renan Sigolo <renan.sigolo@gmail.com>
2021-09-07 15:16:45 +1000

    refact: adjust order of projects

--- components/Projects/projects-data.ts
+++ components/Projects/projects-data.ts
@@ -1,57 +1,52 @@
 export const projectsData = [
   {
-    "title": "AE Study",
-    "subtitle": "Academy of Entrepreneurs Website",
-    "url": "aestudy.com"
+    title: "My ADHD",
+    subtitle: "My ADHD Website and Mobile App",
+    url: "myadhd.app"
   },
   {
-    "title": "Healthy Kids",
-    "subtitle": "Healthy Kids Website",
-    "url": "healthykids.nsw.gov.au"
+    title: "Healthy Kids",
+    subtitle: "Healthy Kids Website",
+    url: "healthykids.nsw.gov.au"
   },
   {
-    "title": "The Constellation Project",
-    "subtitle": "The Constellation Project Website",
-    "url": "theconstellationproject.com.au"
+    title: "The Constellation Project",
+    subtitle: "The Constellation Project Website",
+    url: "theconstellationproject.com.au"
   },
   {
-    "title": "Your Right To Know",
-    "subtitle": "Your Right To Know Website",
-    "url": "yourrighttoknow.com.au"
+    title: "Your Right To Know",
+    subtitle: "Your Right To Know Website",
+    url: "yourrighttoknow.com.au"
   },
   {
-    "title": "Canturi",
-    "subtitle": "Canturi Website",
-    "url": "shop.canturi.com"
+    title: "Canturi",
+    subtitle: "Canturi Website",
+    url: "shop.canturi.com"
   },
   {
-    "title": "TanAndCheek",
-    "subtitle": "Tan+Cheek Website",
-    "url": "tanandcheek.com"
+    title: "TanAndCheek",
+    subtitle: "Tan+Cheek Website",
+    url: "tanandcheek.com"
   },
   {
-    "title": "Healthy Bones Australia",
-    "subtitle": "Healthy Bones Australia Website",
-    "url": "healthybonesaustralia.org.au"
+    title: "Healthy Bones Australia",
+    subtitle: "Healthy Bones Australia Website",
+    url: "healthybonesaustralia.org.au"
   },
   {
-    "title": "Linde",
-    "subtitle": "Linde Material Handling Website",
-    "url": "lindemh.com.au"
+    title: "Live Design",
+    subtitle: "Live Design Website",
+    url: "livedesign.com.br"
   },
   {
-    "title": "My ADHD",
-    "subtitle": "My ADHD Website and Mobile App",
-    "url": "myadhd.app"
+    title: "AE Study",
+    subtitle: "Academy of Entrepreneurs Website",
+    url: "aestudy.com"
   },
   {
-    "title": "Live Design",
-    "subtitle": "Live Design Website",
-    "url": "livedesign.com.br"
-  },
-  {
-    "title": "LEAD",
-    "subtitle": "Kent Leaderboard",
-    "url": "lead.kent.edu.au"
+    title: "LEAD",
+    subtitle: "Kent Leaderboard",
+    url: "lead.kent.edu.au"
   }
-]
+];
