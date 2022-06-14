dd0857ae06aa895ec0c8f8dc9c93a9a060b5ee98
Renan Sigolo <renan.sigolo@gmail.com>
2022-06-15 02:40:18 +1000

    build: fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://renansigolo.com",
   integrations: [react(), sitemap()]
 })
