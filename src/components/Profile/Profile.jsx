a965847fdd175708271358fffd48d32c4d24b152
Renan Sigolo <renan.sigolo@gmail.com>
2022-06-16 01:26:04 +1000

    build: adjust html

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import * as style from "./Profile.module.css"
 
 export default function Profile() {
   return (
-    <div className={style.section} data-test="section-profile">
+    <section className={style.section} data-test="section-profile">
       <img
         alt="Renan Sigolo profile picture"
         className={style.avatar}
@@ -13,6 +13,6 @@ export default function Profile() {
       />
       <h1 className={style.title}>Renan Sigolo</h1>
       <p className={style.subtitle}>Software Engineer</p>
-    </div>
+    </section>
   )
 }
