6571aa6aa5f6c3bab842c8d923cfc66927fe20c2
Renan Sigolo <renan.sigolo@gmail.com>
2022-08-22 19:28:13 +1000

    feat: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
+import partytown from "@astrojs/partytown"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import compress from "astro-compress"
 import { defineConfig } from "astro/config"
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://renansigolo.com",
-  integrations: [react(), sitemap(), compress()]
+  integrations: [
+    react(),
+    sitemap(),
+    compress(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ]
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NEXT_PUBLIC_SITE_URL: string
+  readonly NEXT_PUBLIC_GA_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
