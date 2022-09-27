ad69e3eaf803e0641f318e17467aff6bd9868806
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-27 14:52:32 +1000

    chore: migrate components to astro

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,6 +48,10 @@ module.exports = {
       // Define the configuration for `.astro` file.
       files: ["*.astro"],
       parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"]
+      },
       extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-strict"]
     }
   ]
