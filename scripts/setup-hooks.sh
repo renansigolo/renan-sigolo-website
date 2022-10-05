98a5ccd67fb65aeee4c917dcf3d27430a64de241
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-06 02:40:16 +1100

    fix: dependencies

---
+++ scripts/setup-hooks.sh
@@ -0,0 +1,31 @@
+# Set commit-msg hook
+echo '#!/bin/sh
+npx --no -- commitlint --edit "$1"' >.git/hooks/commit-msg
+chmod +x .git/hooks/commit-msg
+
+# Set pre-commit hook
+echo '#!/bin/sh
+# Lint and format staged files
+linter_exit_code=1
+all_files=$(git diff --staged --name-only --diff-filter=d)
+js_files=$(git diff --staged --name-only --diff-filter=d | grep .js$)
+style_files=$(git diff --staged --name-only --diff-filter=d | grep .{scss|css|astro}$)
+
+./node_modules/.bin/prettier --ignore-unknown --write $all_files &&
+    ./node_modules/.bin/eslint $js_files --quiet --fix &&
+    ./node_modules/.bin/stylelint $style_files --stdin --quiet --fix &&
+    ./node_modules/.bin/prettier --check $all_files
+
+linter_exit_code=$?
+
+git add -f $js_files $style_files
+
+if [ $linter_exit_code -ne 0 ]; then
+    echo "❌ Linter errors have occurred"
+    exit 1
+else
+    echo "✅ Prettier, Eslint and Stylelint did not find any errors"
+    exit 0
+fi
+' >.git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
