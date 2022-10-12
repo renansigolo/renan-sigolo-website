aaf33ca14e0a0f1e5e3a35b720b69b9b3a730def
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-13 01:38:33 +1100

    ci: configure lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,11 +3,6 @@ module.exports = {
     assert: {
       preset: "lighthouse:recommended"
     },
-    collect: {
-      staticDistDir: "./dist",
-      startServerCommand: "npm start",
-      url: ["http://localhost:8080"]
-    },
     upload: {
       target: "temporary-public-storage"
     }
