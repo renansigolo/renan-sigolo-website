7422915e184beb505e3c551210cdde1125c828f9
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-24 16:58:48 +1100

    fix: headers

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 // You can choose which headers to add to the list
 // after learning more below.
-const ContentSecurityPolicy = `
-  base-uri 'none';
-  default-src 'self';
-  object-src 'none';
-  script-src 'none';
-  script-src-elem 'self' https://*.googletagmanager.com 'unsafe-inline';
-  connect-src https://us-central1-renan-sigolo-website.cloudfunctions.net;
-  require-trusted-types-for 'script';
-`
+// const ContentSecurityPolicy = `
+//   base-uri 'none';
+//   default-src 'self';
+//   object-src 'none';
+//   script-src 'none';
+//   script-src-elem 'self' https://*.googletagmanager.com 'unsafe-inline';
+//   connect-src https://us-central1-renan-sigolo-website.cloudfunctions.net;
+//   require-trusted-types-for 'script';
+// `
+
 const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
-  },
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
+  // },
   {
     key: "Referrer-Policy",
     value: "no-referrer-when-downgrade"
