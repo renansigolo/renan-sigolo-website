a57875d4e9aed8b63ffe37640e2d5ad2dc035750
Renan Sigolo <renan.sigolo@gmail.com>
2022-12-21 18:46:30 +1100

    chore: update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
   video: false,
   e2e: {
+    testIsolation: false,
     supportFile: false,
     baseUrl: "http://localhost:3000"
   }
