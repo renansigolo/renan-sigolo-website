3f9d4601c83293aa1d584651856caabc1ceeb50f
Renan Sigolo <renan.sigolo@gmail.com>
2023-09-14 12:21:00 +1000

    chore: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
-import image from "@astrojs/image"
 import partytown from "@astrojs/partytown"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
-import compress from "astro-compress"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
   site: "https://renansigolo.com",
   integrations: [
     react(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
     sitemap(),
-    compress(),
     partytown({
       config: {
         forward: ["dataLayer.push"]
