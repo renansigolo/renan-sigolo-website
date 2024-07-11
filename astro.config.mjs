d297781a050c54a8efc3b0d7dda15bc33b4fbc90
Renan Sigolo <renan.sigolo@gmail.com>
2024-07-11 18:14:59 +1000

    fix: icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import partytown from "@astrojs/partytown"
 import sitemap from "@astrojs/sitemap"
+import icon from "astro-icon"
 import { defineConfig } from "astro/config"
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://renansigolo.com",
   integrations: [
@@ -10,6 +12,9 @@ export default defineConfig({
       config: {
         forward: ["dataLayer.push"]
       }
+    }),
+    icon({
+      iconDir: "src/assets/icons"
     })
   ]
 })
--- src/components/Social/social-data.js
+++ src/components/Social/social-data.js
@@ -1,38 +1,25 @@
-import EnvelopeIcon from "../../assets/icons/envelope-regular.svg"
-import FacebookIcon from "../../assets/icons/facebook.svg"
-import GitHubIcon from "../../assets/icons/github.svg"
-import InstagramIcon from "../../assets/icons/instagram.svg"
-import LinkedInIcon from "../../assets/icons/linkedin.svg"
-import XTwitterIcon from "../../assets/icons/x-twitter.svg"
-
 export const socialData = [
   {
-    icon: GitHubIcon,
     title: "GitHub",
     url: "https://github.com/renansigolo"
   },
   {
-    icon: FacebookIcon,
     title: "Facebook",
     url: "https://facebook.com/renansigoloferreira"
   },
   {
-    icon: LinkedInIcon,
     title: "LinkedIn",
     url: "https://linkedin.com/in/renansigolo"
   },
   {
-    icon: XTwitterIcon,
     title: "Twitter",
     url: "https://twitter.com/renan_sigolo"
   },
   {
-    icon: InstagramIcon,
     title: "Instagram",
     url: "https://instagram.com/renan_sigolo"
   },
   {
-    icon: EnvelopeIcon,
     title: "Email",
     url: "mailto:renan.sigolo@gmail.com?subject=Renan%20Sigolo%20Website%20%7C%20Contact"
   }
